e66e843dd1a9e12399d61b7079406326fc7e3495
saurav526 <sauravtiwari2712005@gmail.com>
2026-04-23 23:34:09 +0530

    update

--- churing model/untitled1.py
+++ churing model/untitled1.py
@@ -21,20 +21,19 @@ y = dataset.iloc[:, 13]
 geography=pd.get_dummies(X["Geography"],drop_first=True)
 gender=pd.get_dummies(X['Gender'],drop_first=True)
 
-## Concatenate the Data Frames
 
 X=pd.concat([X,geography,gender],axis=1)
 
-## Drop Unnecessary columns
+
 X=X.drop(['Geography','Gender'],axis=1)
 
-# Splitting the dataset into the Training set and Test set
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 classifier.save("churn_model.h5")
 
-# Feature Scaling
+
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)

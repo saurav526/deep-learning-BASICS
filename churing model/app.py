3b8239b8b36940c051c6af88261d9cdeb96e155a
saurav526 <sauravtiwari2712005@gmail.com>
2026-04-23 16:23:49 +0530

    update app

--- churing model/app.py
+++ churing model/app.py
@@ -33,7 +33,10 @@ def predict():
         "will_churn": result
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 # To run the app, use the command: python app.py

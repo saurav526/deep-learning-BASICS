db54889b4d0d50feaf743ca720725cd45da05f9d
saurav526 <sauravtiwari2712005@gmail.com>
2026-05-28 15:48:42 +0530

    intial commit

--- GANs/simple.py
+++ GANs/simple.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-# Generator
+# Generator  :- which generate a relalistic data from the actual data
 generator = Sequential([
     Dense(128, activation='relu', input_shape=(100,)),
     Dense(784, activation='sigmoid')
 ])
 
-# Discriminator
+# Discriminator :- which classify the data as real or fake
 discriminator = Sequential([
     Dense(128, activation='relu', input_shape=(784,)),
     Dense(1, activation='sigmoid')
